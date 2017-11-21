6f8448444ba8e53358421c0624161396b507778a
rounddream <pmy000@foxmail.com>
2017-11-22 00:12:20 +0800

    简单表格显示

--- src/components/setting.vue
+++ src/components/setting.vue
@@ -2,6 +2,45 @@
   <div class="wrap">设置
     <h1>{{data.name}}</h1>
     <input v-model="data.name" placeholder="edit me">
+    <div v-if = "show">
+      <ul class="data">
+        <li v-for="item in result">
+          <label>{{ item.title }}:</label>
+          <input type="" name="" v-if="!item.items">
+          <table v-if="item.items">
+              <tr>
+                <th v-for="secItem in item.items.properties">{{ secItem.title }}</th>
+              </tr>
+              <tr>
+                <td v-for="secItem in item.items.properties">
+                  <input type="" name="item.name"  v-if="!secItem.items">
+                </td>
+              </tr>
+            </table>
+        </li>
+      </ul>
+    </div>
+    <!-- <table v-if = "show">
+        <tr>
+          <th v-for="item in result">{{ item.title }}</th>
+        </tr>
+        <tr>
+          <td v-for="item in result">
+           <input type="" name="" v-if="!item.items">
+           <table v-if="item.items">
+              <tr>
+                <th v-for="secItem in item.items.properties">{{ secItem.title }}</th>
+              </tr>
+              <tr>
+                <td v-for="secItem in item.items.properties">
+                  <input type="" name="item.name"  v-if="!secItem.items">
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table> -->
+
     <button @click="handleSubmit">保存</button>
   </div>
 </template>
@@ -12,19 +51,24 @@ export default {
   data() {
     return {
       currentComponent: this.$store.state.currentComponent,
-      schema: null
+      result: null
     };
   },
-  // creat() {
-  //   let name = currentComponent.name
-  //   schema = require('./modules/' + name + '/' + name + 'schema.json')
-  //   console.log("schema", this.schema.type)
-  // },
   computed: {
     data() {
       let { index } = this.currentComponent;
       if (index === undefined) return "";
       return this.$store.state.pageData.components[index].data;
+    },
+    show() {
+      name = this.currentComponent.name
+      //取出本地存储的组件相应的shcema文件
+      let res = JSON.parse(localStorage.getItem('\'' + name +'\''))
+      if(res == null) return false
+      else {
+        this.result = res.properties
+        return true
+      }
     }
   },
   methods: {
@@ -45,5 +89,20 @@ export default {
   position: relative;
   width: 380px;
   background-color: #ede7f6;
+}
+.data {
+  margin: 24px;
+  font-size: 14px;
+  li {
+    margin-bottom: 8px;
+  }
+  table {
+    border: 1px solid #000; 
+    margin-top: 10px;
+    text-align: center;
+  }
+  th {
+    padding: 2px;
+  }
 }
 </style>
