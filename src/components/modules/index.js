03722adb679b1a9439b62eb3d477daebc9d8ce25
rounddream <pmy000@foxmail.com>
2017-12-02 22:22:01 +0800

    修改表格样式

--- src/components/modules/index.js
+++ src/components/modules/index.js
@@ -1,9 +1,11 @@
 import topHeader from './topHeader/topHeader'
 import banner from './banner/banner'
 import list from './list/list'
+import colorPicker from './color-picker/colorPicker'
 
 export default {
   topHeader,
   banner,
-  list
+  list,
+  colorPicker,
 }
